aae17795b5cdf9f3d849d8c8f40ba4aa22da95f7
naplesss <riccardo04naps@gmail.com>
2025-06-13 17:52:41 +0200

    creata l'interfaccia che si collega a partiteweb, l'altra application

---
+++ src/main/java/org/example/matcheweb/proxies/PartiteWeb.java
@@ -0,0 +1,12 @@
+package org.example.matcheweb.proxies;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "PartiteWeb",
+        url = "${name.service.url}",
+        configuration = OpenFeignConfig.class)
+public interface PartiteWeb {
+
+
+}
